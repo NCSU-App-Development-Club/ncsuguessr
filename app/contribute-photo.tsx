2865d132e3c8e5ca610bb26a805a3d169a2cc7d5
Brendon Hablutzel <booklvrbren@gmail.com>
2025-03-25 19:22:32 -0400

    photos

--- app/contribute-photo.tsx
+++ app/contribute-photo.tsx
@@ -1,23 +1,23 @@
 import Text from '../components/global/Text'
 import ScreenView from '../components/global/ScreenView'
 import BackLink from '../components/global/BackLink'
-import React, { useRef } from 'react';
-import { View, Button, TouchableOpacity, Pressable } from 'react-native';
-import { useCameraPermissions, CameraView } from 'expo-camera';
-import * as Location from 'expo-location';
-import { router } from 'expo-router';
-import ScreenButton from '../components/global/ScreenButton';
+import React, { useRef } from 'react'
+import { View, Button, TouchableOpacity, Pressable } from 'react-native'
+import { useCameraPermissions, CameraView } from 'expo-camera'
+import * as Location from 'expo-location'
+import { router } from 'expo-router'
+import ScreenButton from '../components/global/ScreenButton'
 
 export default function ContributePhoto() {
+  const [cameraPermission, requestCameraPermission] = useCameraPermissions()
+  const [locationPermission, requestLocationPermission] =
+    Location.useForegroundPermissions()
 
-  const [cameraPermission, requestCameraPermission] = useCameraPermissions();
-  const [locationPermission, requestLocationPermission] = Location.useForegroundPermissions();
-  
-  const cameraViewRef = useRef<CameraView | null >(null);
+  const cameraViewRef = useRef<CameraView | null>(null)
 
   if (!cameraPermission || !locationPermission) {
     // Permissions are still loading.
-    return <View />;
+    return <View />
   }
 
   if (!cameraPermission.granted) {
@@ -25,9 +25,12 @@ export default function ContributePhoto() {
     return (
       <ScreenView className="items-center justify-center border-4">
         <Text>We need your permission to access the camera.</Text>
-        <ScreenButton onPress={requestCameraPermission} title="Grant Camera Permission" />
+        <ScreenButton
+          onPress={requestCameraPermission}
+          title="Grant Camera Permission"
+        />
       </ScreenView>
-    );
+    )
   }
 
   if (!locationPermission.granted) {
@@ -35,23 +38,43 @@ export default function ContributePhoto() {
     return (
       <ScreenView className="items-center justify-center border-4">
         <Text>We need your permission to access your current location.</Text>
-        <ScreenButton onPress={requestLocationPermission} title="Grant Location Permission" />
+        <ScreenButton
+          onPress={requestLocationPermission}
+          title="Grant Location Permission"
+        />
       </ScreenView>
-    );
+    )
   }
 
   async function takePhoto() {
-    const camera = cameraViewRef.current?._cameraRef.current;
-    if (!camera) return;
-    const picture = await camera.takePicture({});
-    const loc = await Location.getCurrentPositionAsync();
-    router.navigate({pathname: "/contribute-finalize", params: { pictureUri: picture.uri, latitude: loc.coords.latitude, longitude: loc.coords.longitude }});
+    const camera = cameraViewRef.current?._cameraRef.current
+    if (!camera) return
+    const picture = await camera.takePicture({})
+    const loc = await Location.getCurrentPositionAsync()
+    router.navigate({
+      pathname: '/contribute-finalize',
+      params: {
+        pictureUri: picture.uri,
+        latitude: loc.coords.latitude,
+        longitude: loc.coords.longitude,
+      },
+    })
   }
 
   return (
-    <ScreenView className="items-center border-4 w-full flex-1">
-      <CameraView facing="back" ref={cameraViewRef} className="h-full inest-0 justify-end">
-        <ScreenButton onPress={takePhoto} title="Take Photo" />
+    <ScreenView className="">
+      <CameraView
+        facing="back"
+        ref={cameraViewRef}
+        className="absolute top-0 bottom-0 left-0 right-0"
+        zoom={0}
+        ratio="4:3"
+      >
+        <ScreenButton
+          onPress={takePhoto}
+          title="Take Photo"
+          className="h-[75vh] w-screen"
+        />
       </CameraView>
       <BackLink to="/" />
     </ScreenView>
--- components/global/ScreenButton.tsx
+++ components/global/ScreenButton.tsx
@@ -1,14 +1,19 @@
-import { GestureResponderEvent, Pressable, Text } from "react-native"
+import { GestureResponderEvent, Pressable, Text } from 'react-native'
 
 export default function ScreenButton({
   onPress,
-  title
+  title,
+  className,
 }: {
-  onPress: null | ((event: GestureResponderEvent) => void) | undefined,
+  onPress: null | ((event: GestureResponderEvent) => void) | undefined
   title: string
+  className?: string
 }) {
-  return(
-    <Pressable onPress={onPress} className="rounded bg-ncsured w-52 p-1.5 m-1.5">
+  return (
+    <Pressable
+      onPress={onPress}
+      className={`rounded p-1.5 m-1.5 bg-transparent ${className}`}
+    >
       <Text className="text-center text-white font-bold">{title}</Text>
     </Pressable>
   )
