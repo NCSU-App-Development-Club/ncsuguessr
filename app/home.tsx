558306a3e3cbb625b52e642ae75d60c718c39462
nreddy2 <nreddy2@ncsu.edu>
2025-04-08 19:27:32 -0400

    Finished home page

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,12 @@
 import { ImageBackground, View, TouchableOpacity, Linking } from 'react-native';
+import { useRouter } from 'expo-router';
 import Text from '../components/global/Text';
 import ScreenView from '../components/global/ScreenView';
 import BackLink from '../components/global/BackLink';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <ScreenView className="flex-1">
       <ImageBackground
@@ -14,25 +17,32 @@ export default function Home() {
         {/* Header with red background behind text */}
         <View className="absolute top-0 left-0 right-0 w-full">
           <View className="bg-ncsured w-full h-24 flex items-center justify-center">
-            <Text className="text-white text-3xl font-bold">NCSU GUESSER</Text>
+            <Text className="text-white text-3xl font-bold">NCSU GUESSR</Text>
           </View>
         </View>
 
         {/* Center Buttons */}
         <View className="flex-1 justify-end items-center pb-32">
-          <TouchableOpacity 
-            className="bg-ncsured px-12 py-4 rounded-full shadow-xl mb-8"
+          <TouchableOpacity
+            onPress={() => router.push('/game-select')}
+            className="bg-ncsured w-60 py-4 rounded-full shadow-xl mb-8 items-center"
           >
             <Text className="text-white text-xl font-bold">Start Game</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => Linking.openURL('https://example.com/contribute')}>
-            <Text className="text-white text-lg">Contribute to NCSUGuesser!</Text>
+          <TouchableOpacity 
+            onPress={() => router.push('/contribute')}
+            className="bg-ncsured w-60 py-4 rounded-full shadow-xl mb-8 items-center"
+          >
+            <Text className="text-white text-xl font-bold">Contribute</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => Linking.openURL('https://example.com/how-to-play')}>
-            <Text className="text-white text-lg mt-3">How to play?</Text>
-          </TouchableOpacity>
+          {/* <TouchableOpacity 
+            onPress={() => router.push('/contribute')}
+            className="bg-ncsured px-12 py-4 rounded-full shadow-xl mb-8"
+          >
+            <Text className="text-white text-xl font-bold">How to play?</Text>
+          </TouchableOpacity> */}
         </View>
 
         {/* Optional back link */}
