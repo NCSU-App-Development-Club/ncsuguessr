2a9bf11cc43d8a8efcd0523bdffb14e87769cec2
Brendon Hablutzel <booklvrbren@gmail.com>
2025-04-30 22:29:35 -0400

    move over repository types from old backend

---
+++ types/src/games.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+// NOTE: this must be kept in sync with game table definition
+export const GameRow = z.object({
+  id: z.number(),
+  imageId: z.number(),
+  date: z.date(),
+  plays: z.number(),
+  totalDist: z.number(),
+});
+
+export type GameRowType = z.infer<typeof GameRow>;
+
+export const NewGame = GameRow.omit({ id: true }).extend({
+  plays: z.literal(0),
+  totalDist: z.literal(0),
+});
+
+export type NewGameType = z.infer<typeof NewGame>;
+
+export const GameRows = z.array(GameRow);
+
+export type GameRowsType = z.infer<typeof GameRows>;
---
+++ types/src/images.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+// NOTE: this must be kept in sync with image table definition
+export const ImageRow = z.object({
+  id: z.number(),
+  fileLocation: z.string().max(50),
+  latitude: z.number(),
+  longitude: z.number(),
+  takenAt: z.date(),
+  used: z.boolean(),
+  locationName: z.string().max(100),
+  description: z.string(),
+});
+
+export type ImageRowType = z.infer<typeof ImageRow>;
+
+export const NewImage = ImageRow.omit({ id: true });
+
+export type NewImageType = z.infer<typeof NewImage>;
+
+export const ImageRows = z.array(ImageRow);
+
+export type ImageRowsType = z.infer<typeof ImageRows>;
