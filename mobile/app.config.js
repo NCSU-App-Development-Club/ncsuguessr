8a70c1780d3e6d64d342917f3daea341b0de1137
Abel Lu <alu4@ncsu.edu>
2025-05-10 20:11:37 -0400

    android build

---
+++ mobile/app.config.js
@@ -0,0 +1,50 @@
+export default () => ({
+  expo: {
+    scheme: 'ncsuguessr',
+    name: 'ncsuguessr',
+    slug: 'ncsuguessr',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'org.appdevncsu.ncsuguessr',
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      package: 'org.appdevncsu.ncsuguessr',
+      permissions: ['INTERNET'],
+      config: {
+        googleMaps: {
+          apiKey: 'process.env.GOOGLE_MAPS_API_KEY',
+        },
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+      bundler: 'metro',
+    },
+    plugins: ['expo-router'],
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: 'ee047854-2920-4bfe-a221-9827962e63c8',
+      },
+    },
+  },
+})
