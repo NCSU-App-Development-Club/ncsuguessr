5ce89cc35596ceafde67eb8b23cb5abf1422cdb5
Abel Lu <alu4@ncsu.edu>
2025-05-11 18:43:05 -0400

    icon stuff

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -25,7 +25,7 @@ export default () => ({
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
+        foregroundImage: './assets/wolf.png',
         backgroundColor: '#ffffff',
       },
       package: 'org.appdevncsu.ncsuguessr',
