93ffa726d3206439e141ffdb38c5928e434a86f2
Tyler Swanson <31103658+ex4dev@users.noreply.github.com>
2025-09-14 22:42:49 -0400

    Expo SDK 54 (#65)
    * Upgrade to Expo SDK 54
    
    * Use react-native-safe-area-context
    
    * Upgrade dependencies

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Slot } from 'expo-router'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import '../global.css'
 
 export default function Layout() {
