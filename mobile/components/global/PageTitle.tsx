18cc1e89d1bed7b940d4246c0ea3343cbddbb668
Cole Hincken <colehincken@gmail.com>
2025-10-09 13:07:19 -0400

    Update PageTitle.tsx

--- mobile/components/global/PageTitle.tsx
+++ mobile/components/global/PageTitle.tsx
@@ -1,26 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleProp, TextStyle } from "react-native";
 
 interface PageTitleProperties {
   text: string;
-  color?: string;
-  fontSize?: number;
-  fontWeight?: string;
-  style?: object;
 }
 
-function PageTitle({
-  text,
-  color = "#000000",
-  fontSize = 32,
-  fontWeight = "bold",
-  style = {},
-}: PageTitleProperties) {
+function PageTitle({ text }: PageTitleProperties) {
   return (
     <View className="flex-1 justify-center items-center">
       <Text
         className="text-center"
-        style={[{ color, fontSize, fontWeight }, style]}
+        style={[
+          {
+            color: "#000000",
+            fontSize: 32,
+            fontWeight: "bold",
+          },
+        ]}
       >
         {text}
       </Text>
