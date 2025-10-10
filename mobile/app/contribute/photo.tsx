5a6c91050ebef029697ef0e35877f7a21b411792
ewschofi <ewschofi@ncsu.edu>
2025-10-09 20:14:10 -0400

    Added missing back button to Take Photo screen

--- mobile/app/contribute/photo.tsx
+++ mobile/app/contribute/photo.tsx
@@ -19,7 +19,10 @@ export default function ContributePhoto() {
 
   if (!cameraPermission || !locationPermission) {
     // Permissions are still loading.
-    return <View />
+    return <View>
+      <BackLink to="/" />
+    </View>
+
   }
 
   if (!cameraPermission.granted) {
@@ -31,6 +34,7 @@ export default function ContributePhoto() {
           onPress={requestCameraPermission}
           title="Grant Camera Permission"
         />
+        <BackLink to="/" />
       </ScreenView>
     )
   }
@@ -44,6 +48,7 @@ export default function ContributePhoto() {
           onPress={requestLocationPermission}
           title="Grant Location Permission"
         />
+        <BackLink to="/" />
       </ScreenView>
     )
   }
